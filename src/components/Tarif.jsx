287f8910df3e2b04cc19f9daa77236633db14ebd
ararodina <ararodina@srt.beeline.ru>
2024-08-14 19:21:47 +0400

    feat: add tariff selection functionality on click

--- src/components/Tarif.jsx
+++ src/components/Tarif.jsx
@@ -1,20 +1,26 @@
-import styles from './Tarif.module.css';
-export default function Tarif({ title, price, speed, clarification }) {
+import React from 'react';
 
+import styles from './Tarif.module.css';
+export default function Tarif({ title, price, speed, clarification, isActive, onClick  }) {
     const tarifStyle = {
         backgroundColor: price === 300 ? 'rgb(46, 129, 129)' :
                          price === 450 ? 'green' :
                          price === 550 ? 'rgba(255, 0, 0, 0.8)' : 
                          price === 1000 ? 'black' : 
                          'black',
-        boxShadow: price === 550 ? '0 0 10px rgba(255, 0, 0, 0.6)' : 'none', 
-        transform: price === 550 ? 'scale(1.05)' : 'scale(1)',
+        boxShadow: price === 550 ? '0 0 10px rgba(255, 0, 0, 0.6)' : 'none',
+    //    transform: price === 550 ? 'scale(1.05)' : 'scale(1)', // нужно чтобы выделить тариф 550*/ 
     };
-
+    const activeTarifStyle={
+    transform: price === 550 ? 'scale(1.05)' : 'scale(1)'
+    }
 
     return (
-        <main className={styles.tarif} >
-            <div className={styles.tarif_body}>
+        <main
+        className={`${styles.tarif} ${isActive ? styles.active : ''}`} // Добавляем класс 'active' если тариф активен
+        onClick={onClick}
+    >
+            <div className={styles.tarif_body} style={{activeTarifStyle}} >
                 <div className={styles.tarif_part1} style={tarifStyle}>
                     <h3 className={styles.tarif_title}>{title}</h3>
                     <div className={styles.tarif_price_container}>
--- src/components/TarifList.jsx
+++ src/components/TarifList.jsx
@@ -1,38 +1,44 @@
 // TarifList.js
-import React from 'react';
+import React, { useState } from 'react';
 import Tarif from './Tarif';
 
 import styles from './TarifList.module.css';
 
+const tarifData = [{title:"Безлимитный 300", price:300, speed:"до 10 Мбит/сек",
+clarification:"Объем включенного трафика не ограничен"},{title:"Безлимитный 450", price:450, speed:"до 50 Мбит/сек",
+    clarification:"Объем включенного трафика не ограничен"},{title:"Безлимитный 550", price:550, speed:"до 100 Мбит/сек",
+        clarification:"Объем включенного трафика не ограничен"},{title:"Безлимитный 1000", price:1000, speed:"до 200 Мбит/сек",
+            clarification:"Объем включенного трафика не ограничен"}]
+
 function TarifList() {
+
+    const [activeTarifIndex, setActiveTarifIndex] = useState(null);
+
+    const handleTarifClick = (index) => {
+        // Меняем состояние на текущий индекс или сбрасываем, если он уже активен
+        setActiveTarifIndex(activeTarifIndex === index ? null : index);
+    };
+
+
+
     return (
-        // Можно использовать React.Fragment
         <React.Fragment>
             <div className={styles.tarif_container}>
-            <Tarif
-                title="Безлимитный 300"
-                price={300}
-                speed="до 10 Мбит/сек"
-                clarification="Объем включенного трафика не ограничен"
-            />
-            <Tarif
-                title="Безлимитный 450"
-                price={450}
-                speed="до 50 Мбит/сек"
-                clarification="Объем включенного трафика не ограничен"
-            />
-            <Tarif
-                title="Безлимитный 550"
-                price={550}
-                speed="до 100 Мбит/сек"
-                clarification="Объем включенного трафика не ограничен"
-            />
-            <Tarif
-                title="Безлимитный 1000"
-                price={1000}
-                speed="до 200 Мбит/сек"
-                clarification="Объем включенного трафика не ограничен"
-            />
+            {tarifData.map((item,index)=>{
+                return (
+                    <Tarif
+                    key={index} {...item} //запись короче
+                    //более объмный вариант
+                    /*title={item.title}
+                    price={item.price}
+                    speed={item.speed}
+                    clarification={item.clarification}*/
+                    isActive={activeTarifIndex === index} // Передаем активное состояние
+                            onClick={() => handleTarifClick(index)} 
+                    />
+
+                )
+            })}
             </div>
         </React.Fragment>
     );
